2c4fd34c3c679dc708625160204a10d5e3ae2ca1
evanhauzal <evanhauzal@gmail.com>
2026-06-25 16:02:51 +0700

    sparkkk

--- scripts/start_spark_master.bat
+++ scripts/start_spark_master.bat
@@ -6,5 +6,9 @@ echo ========================================================
 set SPARK_LOCAL_IP=192.168.1.10
 echo [*] SPARK_LOCAL_IP disetel ke %SPARK_LOCAL_IP%
 
+echo [*] Mencari lokasi instalasi PySpark di Python...
+FOR /F "tokens=*" %%i IN ('python -c "import pyspark; import os; print(os.path.dirname(pyspark.__file__))"') DO set SPARK_HOME=%%i
+echo [*] SPARK_HOME ditemukan di: %SPARK_HOME%
+
 echo [*] Menjalankan Master (Port 7077, WebUI Port 8080)...
-spark-class org.apache.spark.deploy.master.Master --ip 192.168.1.10 --port 7077 --webui-port 8080
+"%SPARK_HOME%\bin\spark-class2.cmd" org.apache.spark.deploy.master.Master --ip 192.168.1.10 --port 7077 --webui-port 8080
--- scripts/start_spark_worker.bat
+++ scripts/start_spark_worker.bat
@@ -10,4 +10,8 @@ echo [*] SPARK_LOCAL_IP disetel ke %SPARK_LOCAL_IP%
 set MASTER_URL=spark://192.168.1.10:7077
 echo [*] Menghubungkan ke Master %MASTER_URL%...
 
-spark-class org.apache.spark.deploy.worker.Worker %MASTER_URL% --webui-port 8081
+echo [*] Mencari lokasi instalasi PySpark di Python...
+FOR /F "tokens=*" %%i IN ('python -c "import pyspark; import os; print(os.path.dirname(pyspark.__file__))"') DO set SPARK_HOME=%%i
+echo [*] SPARK_HOME ditemukan di: %SPARK_HOME%
+
+"%SPARK_HOME%\bin\spark-class2.cmd" org.apache.spark.deploy.worker.Worker %MASTER_URL% --webui-port 8081
